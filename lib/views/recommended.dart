c2a3fb744e263fdae530f57dc551427312431220
jf-duque <53950971+jf-duque@users.noreply.github.com>
2023-10-12 22:06:32 -0500

    correccion search bar recommended

--- lib/views/recommended.dart
+++ lib/views/recommended.dart
@@ -3,32 +3,47 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:unishop/models/product.dart';
 import 'package:unishop/repositories/posts_repository.dart';
 
-class RecommendedView extends StatelessWidget {
+class RecommendedView extends StatefulWidget {
+  @override
+  _RecommendedViewState createState() => _RecommendedViewState();
+}
+
+class _RecommendedViewState extends State<RecommendedView> {
+  late Future<List<Product>> _productsFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _productsFuture = fetchProducts();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-         appBar: AppBar(
+        appBar: AppBar(
+          toolbarHeight: 80,
           automaticallyImplyLeading: false,
+          backgroundColor: Colors.white,
           title: Container(
             padding: EdgeInsets.symmetric(horizontal: 10.0),
-            margin:
-                EdgeInsets.fromLTRB(8.0, 10.0, 8.0, 0), // Add top margin here
+            margin: EdgeInsets.fromLTRB(8.0, 10.0, 8.0, 0),
             decoration: BoxDecoration(
               color: Colors.grey[300],
               borderRadius: BorderRadius.circular(10.0),
             ),
             child: Row(
               children: [
-                //Use SvgPicture.asset to display the SVG icon
                 Padding(
-                  padding: EdgeInsets.only(left: 25.0), // Add left padding here
+                  padding: EdgeInsets.only(left: 25.0),
                   child: SvgPicture.asset(
-                    'assets/Favorite.svg', // Replace with your SVG file path
-                    width: 12, // Set the width of the SVG icon
-                    height: 12, // Set the height of the SVG icon
-                    colorFilter: ColorFilter.mode(Colors.black,
-                        BlendMode.srcIn), // Set the color of the SVG icon
+                    'assets/Favorite.svg',
+                    width: 12,
+                    height: 12,
+                    colorFilter: ColorFilter.mode(
+                      Colors.black,
+                      BlendMode.srcIn,
+                    ),
                   ),
                 ),
                 SizedBox(width: 10.0),
@@ -45,26 +60,24 @@ class RecommendedView extends StatelessWidget {
             ),
           ),
         ),
-        body:
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: FutureBuilder<List<Product>>(
-              future: fetchProducts(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return CircularProgressIndicator();
-                } else if (snapshot.hasError) {
-                  return Text('Error: ${snapshot.error}');
-                } else if (!snapshot.hasData) {
-                  return Text('No se encontraron datos');
-                } else {
-                  final products = snapshot.data!;
-                  return ProductCatalog(products: products);
-                }
-              },
-            ),
-          )
-         
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: FutureBuilder<List<Product>>(
+            future: _productsFuture,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasError) {
+                return Center(child: Text('Error: ${snapshot.error}'));
+              } else if (!snapshot.hasData) {
+                return Center(child: Text('No se encontraron datos'));
+              } else {
+                final products = snapshot.data!;
+                return ProductCatalog(products: products);
+              }
+            },
+          ),
+        ),
       ),
     );
   }
@@ -78,16 +91,16 @@ class ProductCatalog extends StatelessWidget {
   final List<Product> products;
 
   ProductCatalog({required this.products});
+
   bool _isValidImageUrl(String imageUrl) {
     return imageUrl.startsWith('h'.trim());
   }
-
 
   @override
   Widget build(BuildContext context) {
     return GridView.builder(
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2, // Dos productos por fila
+        crossAxisCount: 2,
         crossAxisSpacing: 8.0,
         mainAxisSpacing: 8.0,
       ),
@@ -97,39 +110,29 @@ class ProductCatalog extends StatelessWidget {
         return Card(
           elevation: 5,
           shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10.0), // Ajusta el radio según tus preferencias
+            borderRadius: BorderRadius.circular(10.0),
           ),
-            child: SizedBox(
-              height: 00,
-              child: Column(
-                children: [
-                  if (_isValidImageUrl(product.image.first))
-                    Image.network(
-                      product.image.first,
-                      fit: BoxFit.cover,
-                      height: 100,
-                      width: double.infinity,
-                    )
-                  else
-                    Image.asset(
-                      'assets/NotFound.png', // Replace with the path to your placeholder image
-                      fit: BoxFit.cover,
-                      height: 100,
-                      width: double.infinity,
-                    ),
-                  // Padding(
-                  //   padding: EdgeInsets.symmetric(horizontal: 16, vertical: 1),
-                  //   child: Text(
-                  //     product.price,
-                  //     style: TextStyle(
-                  //       fontWeight: FontWeight.bold,
-                  //       fontSize: 16,
-                  //     ),
-                  //   ),
-                  // ),
-                ],
-              ),
-            )
+          child: SizedBox(
+            height: 00,
+            child: Column(
+              children: [
+                if (_isValidImageUrl(product.image.first))
+                  Image.network(
+                    product.image.first,
+                    fit: BoxFit.cover,
+                    height: 100,
+                    width: double.infinity,
+                  )
+                else
+                  Image.asset(
+                    'assets/NotFound.png',
+                    fit: BoxFit.cover,
+                    height: 100,
+                    width: double.infinity,
+                  ),
+              ],
+            ),
+          ),
         );
       },
     );
