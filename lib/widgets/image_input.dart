540f8cc01bf3c259f2e1e8af799112a92bbc3377
SAPRIAL <s.prietoa@uniandes.edu.co>
2023-10-25 13:04:25 -0500

    Create Post View Fixed

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -61,7 +61,7 @@ class _ImageInputState extends State<ImageInput> {
           color: Theme.of(context).colorScheme.primary.withOpacity(0.2),
         ),
       ),
-      height: 250,
+      height: 180,
       width: double.infinity,
       alignment: Alignment.center,
       child: content,
