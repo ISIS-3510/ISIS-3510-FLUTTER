7eb37705383ff5505cfa0542d736be0629ab4ff2
Edd1e <estebanrco@hotmail.com>
2023-11-06 01:56:58 -0500

    this is a solution to the problem, not the best, but is a solution

--- lib/View/home.dart
+++ lib/View/home.dart
@@ -36,7 +36,7 @@ class _HomeViewState extends State<HomeView> {
                 if (snapshot.connectionState == ConnectionState.waiting) {
                   return Center(child: CircularProgressIndicator());
                 } else if (snapshot.hasError) {
-                  return Text('Error: ${snapshot.error}');
+                  return NoInternet();
                 } else if (!snapshot.hasData) {
                   return Text('No se encontraron datos');
                 } else {
@@ -55,7 +55,7 @@ class _HomeViewState extends State<HomeView> {
   Future<List<ProductDTO>> fetchProducts() async {
     HomeController controller = HomeController();
     return controller.getListProducts();
-  }
+  }     
 
   void showAlert(String title, String message, Color backgroundColor) {
     showDialog(
@@ -93,4 +93,48 @@ class _HomeViewState extends State<HomeView> {
     );
   }
 
+}
+
+class NoInternet extends StatefulWidget {
+  @override
+  _NoInternetState createState() => _NoInternetState();
+}
+
+class _NoInternetState extends State<NoInternet> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+            child: Text(
+              'Uh oh! Check if you have an internet connection',
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Colors.black,
+                fontWeight: FontWeight.w900,
+                fontSize: 20,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+            child: Text(
+              'Once you have an internet connection, your products will be loaded again',
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                color: Colors.black,
+                fontSize: 15,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
