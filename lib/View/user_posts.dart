e5b94c6d1dbe46b662a0baaabf251166acdb5224
SAPRIAL <s.prietoa@uniandes.edu.co>
2023-12-03 21:17:41 -0500

    Sold products functionality and view done

--- lib/View/user_posts.dart
+++ lib/View/user_posts.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:decimal/decimal.dart';
 import 'package:decimal/intl.dart';
@@ -19,12 +20,12 @@ class UserPostsView extends StatefulWidget {
 
 class _UserPostsViewState extends State<UserPostsView> {
   List<ProductDTO> _products = [];
+  List<ProductDTO> _soldProducts = [];
   UserPostController controller = UserPostController();
   StreamSubscription? listener;
   InternetConnectionChecker? customInstance;
   bool? isInternet;
   bool isLoading = true;
-  
 
   @override
   void initState() {
@@ -34,23 +35,24 @@ class _UserPostsViewState extends State<UserPostsView> {
       checkInterval: const Duration(seconds: 1), // Custom check interval
     );
     listener = customInstance!.onStatusChange.listen((status) async {
-    switch (status) {
-      case InternetConnectionStatus.connected:
-        await _loadProducts();
-        print('Data connection is available.');
-        setState(() {
-          isInternet = true;
-          isLoading = false;
-        });
-        break;
-      case InternetConnectionStatus.disconnected:
-        setState(() {
-          _products = [];
-          isInternet = false;
-          isLoading = false;
-        });
-        print('You are disconnected from the internet.');
-        break;
+      switch (status) {
+        case InternetConnectionStatus.connected:
+          await _loadProducts();
+          await _loadSoldProducts();
+          print('Data connection is available.');
+          setState(() {
+            isInternet = true;
+            isLoading = false;
+          });
+          break;
+        case InternetConnectionStatus.disconnected:
+          setState(() {
+            _products = [];
+            isInternet = false;
+            isLoading = false;
+          });
+          print('You are disconnected from the internet.');
+          break;
       }
     });
   }
@@ -65,6 +67,18 @@ class _UserPostsViewState extends State<UserPostsView> {
     final loadedProducts = await controller.loadProducts();
     setState(() {
       _products = loadedProducts;
+    });
+  }
+
+  Future<void> _loadSoldProducts() async {
+    final List<ProductDTO> loadedSoldProducts = [];
+    for (final product in _products) {
+      if (product.isSold == true) {
+        loadedSoldProducts.add(product);
+      }
+    }
+    setState(() {
+      _soldProducts = loadedSoldProducts;
     });
   }
 
@@ -100,6 +114,7 @@ class _UserPostsViewState extends State<UserPostsView> {
   @override
   Widget build(BuildContext context) {
     Widget? content;
+    Widget? content2;
 
     if (_products.isEmpty && isInternet == true && isLoading == false) {
       content = Center(
@@ -128,16 +143,13 @@ class _UserPostsViewState extends State<UserPostsView> {
           mainAxisSize: MainAxisSize.min,
           children: [
             Padding(
-              padding: const EdgeInsets.fromLTRB(10,0,10,0),
-              child: Text(
-                'Uh oh! Check if you have internet connection',
-                textAlign: TextAlign.center,
-                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+              padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+              child: Text('Uh oh! Check if you have internet connection',
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.titleLarge!.copyWith(
                       color: Colors.black,
                       fontWeight: FontWeight.w900,
-                      fontSize: 20
-                    )
-                ),
+                      fontSize: 20)),
             ),
             const SizedBox(
               height: 16,
@@ -147,17 +159,16 @@ class _UserPostsViewState extends State<UserPostsView> {
               child: Text(
                 'Once you have internet connection, your products will be loaded again',
                 textAlign: TextAlign.center,
-                style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                      color: Colors.black,
-                      fontSize: 15
-                    ),
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyLarge!
+                    .copyWith(color: Colors.black, fontSize: 15),
               ),
             ),
           ],
         ),
       );
-    }
-    else if (_products.isNotEmpty && isLoading == false) {
+    } else if (_products.isNotEmpty && isLoading == false) {
       content = ListView.builder(
         padding: EdgeInsets.all(8),
         itemCount: _products.length,
@@ -214,36 +225,256 @@ class _UserPostsViewState extends State<UserPostsView> {
               ),
               Container(
                 color: Color.fromARGB(255, 217, 217, 217),
-                height: 25,
+                height: 15,
+              ),
+              Container(
+                color: Color.fromARGB(255, 217, 217, 217),
+                height: 30,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    IconButton(
+                      icon: FaIcon(
+                        _products[index].isSold!
+                            ? FontAwesomeIcons.solidCheckCircle
+                            : FontAwesomeIcons.solidTimesCircle,
+                        color: Colors.black,
+                        size: 20,
+                      ),
+                      onPressed: () async {
+                        if (_products[index].isSold!) {
+                          await UserPostController()
+                              .unsoldProduct(_products[index].id!);
+                          setState(() {
+                            _products[index].isSold = false;
+                          });
+                        } else {
+                          await UserPostController()
+                              .soldProduct(_products[index].id!);
+                          setState(() {
+                            _products[index].isSold = true;
+                          });
+                        }
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                color: Color.fromARGB(255, 217, 217, 217),
+                height: 15,
               ),
             ],
           ),
         ),
       );
-    } else if (isLoading == true && isInternet == null){
+    } else if (isLoading == true && isInternet == null) {
       content = Center(
-      child: CircularProgressIndicator(
-        color: Colors.black,
+        child: CircularProgressIndicator(
+          color: Colors.black,
         ),
       );
     }
 
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        foregroundColor: Colors.black,
-        title: Text(
-          'Your Publications',
-          style: Theme.of(context).textTheme.titleLarge,
+    if (_soldProducts.isEmpty && isInternet == true && isLoading == false) {
+      content2 = Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'Uh oh ... nothing here!',
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            Text(
+              'Try adding a post',
+              style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                    color: Colors.black,
+                  ),
+            ),
+          ],
         ),
-        actions: [
-          IconButton(
-            onPressed: _addProduct,
-            icon: const Icon(Icons.add),
+      );
+    } else if (_soldProducts.isEmpty && isInternet == false && isLoading == false) {
+      content2 = Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+              child: Text('Uh oh! Check if you have internet connection',
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w900,
+                      fontSize: 20)),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+              child: Text(
+                'Once you have internet connection, your products will be loaded again',
+                textAlign: TextAlign.center,
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyLarge!
+                    .copyWith(color: Colors.black, fontSize: 15),
+              ),
+            ),
+          ],
+        ),
+      );
+    } else if (_soldProducts.isNotEmpty && isLoading == false) {
+      content2 = ListView.builder(
+        padding: EdgeInsets.all(8),
+        itemCount: _soldProducts.length,
+        itemBuilder: (ctx, index) => Card(
+          margin: const EdgeInsets.all(8),
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+              side: BorderSide(color: Color.fromARGB(20, 17, 19, 21))),
+          clipBehavior: Clip.hardEdge,
+          elevation: 5,
+          child: Column(
+            children: [
+              Image.network(_soldProducts[index].image.first),
+              Container(
+                color: Color.fromARGB(255, 217, 217, 217),
+                height: 25,
+              ),
+              Container(
+                color: Color.fromARGB(255, 217, 217, 217),
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(16, 0, 0, 0),
+                      child: Text(
+                        _soldProducts[index].title,
+                        style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                              fontSize: 25,
+                              fontWeight: FontWeight.w700,
+                            ),
+                        textAlign: TextAlign.left,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                color: Color.fromARGB(255, 217, 217, 217),
+                height: 15,
+              ),
+              Container(
+                color: Color.fromARGB(255, 217, 217, 217),
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(16, 0, 0, 0),
+                      child: Text(
+                        "\$ ${formatMoney(_soldProducts[index].price.toString())}",
+                        style: Theme.of(context).textTheme.titleLarge,
+                        textAlign: TextAlign.left,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                color: Color.fromARGB(255, 217, 217, 217),
+                height: 15,
+              ),
+              Container(
+                color: Color.fromARGB(255, 217, 217, 217),
+                height: 30,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    IconButton(
+                      icon: FaIcon(
+                        _soldProducts[index].isSold!
+                            ? FontAwesomeIcons.solidCheckCircle
+                            : FontAwesomeIcons.solidTimesCircle,
+                        color: Colors.black,
+                        size: 20,
+                      ),
+                      onPressed: () async {
+                        if (_soldProducts[index].isSold!) {
+                          await UserPostController()
+                              .unsoldProduct(_soldProducts[index].id!);
+                          setState(() {
+                            _soldProducts[index].isSold = false;
+                          });
+                        } else {
+                          await UserPostController()
+                              .soldProduct(_soldProducts[index].id!);
+                          setState(() {
+                            _soldProducts[index].isSold = true;
+                          });
+                        }
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                color: Color.fromARGB(255, 217, 217, 217),
+                height: 15,
+              ),
+            ],
           ),
-        ],
+        ),
+      );
+    } else if (isLoading == true && isInternet == null) {
+      content2 = Center(
+        child: CircularProgressIndicator(
+          color: Colors.black,
+        ),
+      );
+    }
+
+    return DefaultTabController(
+      length: 2,
+      initialIndex: 0,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+          title: Text(
+            'Your Publications',
+            style: Theme.of(context).textTheme.titleLarge,
+          ),
+          bottom: TabBar(
+            labelStyle: Theme.of(context).textTheme.titleLarge,
+            labelColor: Colors.black,
+            tabs: [
+              Tab(
+                text: 'All your posts',
+              ),
+              Tab(
+                text: 'All your sold posts',
+              )
+            ],
+            onTap: (index) {
+              if (index == 0 && isInternet == true) {
+                _loadProducts();
+              } else if (index == 1 && isInternet == true) {
+                _loadSoldProducts();
+              }
+            },
+          ),
+          actions: [
+            IconButton(
+              onPressed: _addProduct,
+              icon: const Icon(Icons.add),
+            ),
+          ],
+        ),
+        body: TabBarView(children: [content!,content2!]) ,
       ),
-      body: content,
     );
   }
 }
