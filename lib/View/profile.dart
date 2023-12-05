78f43aaa5e359b891d16269dd0b27d4f55176cb2
SAPRIAL <s.prietoa@uniandes.edu.co>
2023-12-04 20:44:51 -0500

    Micro-optimizations

--- lib/View/profile.dart
+++ lib/View/profile.dart
@@ -12,7 +12,7 @@ import 'package:unishop/Model/DAO/dao.dart';
 
 class Profile extends StatefulWidget {
   @override
-  _ProfileState createState() => _ProfileState();
+  State<Profile> createState() => _ProfileState();
 }
 
 class _ProfileState extends State<Profile> {
@@ -79,7 +79,7 @@ class _ProfileState extends State<Profile> {
       builder: (BuildContext context) {
         return AlertDialog(
           title: Text(title),
-          content: Container(
+          content: SizedBox(
             width: 100,
             height: 40,
             child: SingleChildScrollView(
@@ -218,12 +218,12 @@ class _ProfileState extends State<Profile> {
                 Expanded(
                   child: ElevatedButton(
                     onPressed: (){ getCurrentLocation();}, // Call the help function here
-                    child: Text('HELP!'),
                     style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.amber,
                       foregroundColor: Colors.black,
                       padding: EdgeInsets.symmetric(vertical: 15),
                     ),
+                    child: Text('HELP!'),
                   ),
                 ),
               ],
@@ -237,12 +237,12 @@ class _ProfileState extends State<Profile> {
                       Navigator.push(context,
                       MaterialPageRoute(
                         builder: (context) => BugReporterView()));},
-                    child: Text('Report a problem'),
                     style: ElevatedButton.styleFrom(
-                      primary: Colors.amber,
-                      onPrimary: Colors.black,
+                      backgroundColor: Colors.amber,
+                      foregroundColor: Colors.black,
                       padding: EdgeInsets.symmetric(vertical: 15),
                     ),
+                    child: Text('Report a problem'),
                   ),
                 ),
               ],
@@ -285,8 +285,8 @@ class _ProfileState extends State<Profile> {
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: Text("Log out"),
-          content: Text("Are you sure you want to log out?"),
+          title: Text("Log out", style: TextStyle(color: Colors.black)),
+          content: Text("Are you sure you want to log out?", style: TextStyle(color: Colors.black)),
           backgroundColor: Colors.white, // Establece el color de fondo
           actions: <Widget>[
             TextButton(
@@ -300,7 +300,7 @@ class _ProfileState extends State<Profile> {
               },
             ),
             TextButton(
-              child: Text('Cancel'),
+              child: Text('Cancel', style: TextStyle(color: Colors.black)),
               onPressed: () {
                 Navigator.of(context).pop(); // Cierra la alerta
               },
